8efb0a1ce42b802c9f81760284b82b3ce33f3250
Piyush Motwani <piyushmotwani211@gmail.com>
2023-08-02 16:22:50 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -225,7 +225,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://stackoverflow-gaurav-tejwani.vercel.app/",
     credentials: true,
   })
 );
